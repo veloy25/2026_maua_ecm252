bb9acba7ddfdaa2a09e411c20a1dbff935a51af9
veloy25 <veloy.devmaua@gmail.com>
2026-03-07 10:40:11 -0300

    learn(ap03-react): encerra a apostila

--- ap003_react_jsx_componentes/src/App.jsx
+++ ap003_react_jsx_componentes/src/App.jsx
@@ -1,13 +1,26 @@
+import './styles.css'
+
 const App = () => {
+
+  const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', borderStyle: 'hidden', color:'white', width: '100%', borderRadius: 8}
+
+  const textDoRotulo = 'Nome:'
+
+  const obterTextoDoBotao = () => 'Enviar'
+
+  const aoClicar = () => alert('Clicou')
+
+
   return <div style={{margin: 'auto', width: 576, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
-    <label htmlFor="campoNome" style={{display: 'block', marginBottom: 8}}>
-      Nome:
+    <label className= "rotulo" htmlFor="campoNome" style={{display: 'block', marginBottom: 8}}>
+      {textDoRotulo}
     </label>
 
     <input type="text" id="campoNome" style={{paddingTop: 8, paddingBottom: 8, width: '100%', borderStyle: 'hidden', outiline:'none', borderRadius: 8}}/>
 
-    <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', borderStyle: 'hidden', color:'white', width: '100%', borderRadius: 8}}>
-      Enviar
+    <button style={estilosBotao} onClick = {() => aoClicar()}>
+      {obterTextoDoBotao()}
+      
     </button>
 
   </div>
